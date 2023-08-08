aa7852e80bede2014662a279ce7f1fb8505b2058
Daniel Mureb <danielmureb@hotmail.com>
2023-08-08 09:08:38 -0300

    operadores novos

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,6 +1,7 @@
 a = 3
 b = -4
 
+# Operadores Aritméticos
 soma = a + b
 sub = a - b
 mult = a * b
@@ -14,3 +15,60 @@ print(f"Divisão: { div }")
 print(f"Divisão de Inteiros: { div_inteiros }")
 print(f"Cálculos dentro do print: { a * b }")
 print(f"Resto da divisão de 16 por a: { 16 % a }")
+
+# Operadores Relacionais
+print(f"a == b: { a == b }")
+print(f"a < 5: { a < 5 }")
+print(f"a > b: { a > b }")
+print(f"a <= 3: { a <= 3 }")
+print(f"a >= 4: { a >= 4 }")
+print(f"a != b: { a != b }")
+
+'''
+== Igual a
+< Menor que
+> Maior que
+<= Menor ou igual
+>= Maior ou igual
+!= Diferente de
+'''
+
+c = a != b
+print(f"c: {c}")
+print(f"Tipo da var c: {type(c)}") # bool é o tipo booleano ou lógico
+
+# Operadores Lógicos
+logic1 = True
+logic2 = False
+print(type(logic1))
+print(type(logic2))
+
+print(f"logic1: {logic1}")
+print(f"not logic1: {not logic1}")
+print(f"logic1 or logic2: {logic1 or logic2}")
+print(f"logic1 and logic2: {logic1 and logic2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
